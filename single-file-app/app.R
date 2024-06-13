98087c783b8a58052d43e685aec32baf633b1525
MerrittDeltaCouncil <annie.merritt@deltacouncil.ca.gov>
2024-06-13 08:59:12 -0700

    created a 1-file app with scrath folder for plots

--- single-file-app/app.R
+++ single-file-app/app.R
@@ -1,9 +1,49 @@
+# load packages ----
 library(shiny)
+library(palmerpenguins)
+library(tidyverse)
+
 # user interface ----
-ui <- fluidPage()
+ui <- fluidPage(  
+  # app title ----
+  tags$h1("My App Title"), # alternatively, you can use the `h1()` wrapper function
+  
+  # app subtitle ----
+  h4(strong("Exploring Antarctic Penguin Data")), # alternatively, `tags$h4(tags$strong("text"))`
+
+  # body mass slider ----
+  sliderInput(inputId = "body_mass_input", label = "Select a range of body masses (g):",
+              min = 2700, max = 6300, value = c(3000, 4000)),
+  
+  # body mass plot output ----
+  plotOutput(outputId = "bodyMass_scatterplot_output")
+  
+)
 
 # server instructions ----
-server <- function(input, output) {}
+server <- function(input, output) {
+  
+  # filter body masses ----
+  body_mass_df <- reactive({ 
+    penguins %>%  
+      filter(body_mass_g %in% c(input$body_mass_input[1]:input$body_mass_input[2])) 
+  })
+  
+  # render penguin scatter plot ----
+  output$bodyMass_scatterplot_output <- renderPlot({ 
+    ggplot(na.omit(body_mass_df()),
+           aes(x = flipper_length_mm, y = bill_length_mm,
+               color = species, shape = species)) + 
+      geom_point() + 
+      scale_color_manual(values = c("Adelie" = "#FEA346", "Chinstrap" = "#B251F1", "Gentoo" = "#4BA4A4")) +
+      scale_shape_manual(values = c("Adelie" = 19, "Chinstrap" = 17, "Gentoo" = 15)) +
+      labs(x = "Flipper length (mm)", y = "Bill length (mm)", 
+           color = "Penguin species", shape = "Penguin species") + 
+      theme_minimal() + 
+      theme(legend.position = c(0.85, 0.2), 
+            legend.background = element_rect(color = "white")) 
+  })
+}
 
 # combine UI & server into an app ----
 shinyApp(ui = ui, server = server)
